195e5c86e32b720df4400bd7c0463ffb161bd3e1
dpeter99 <dpeter99@gmail.com>
2024-11-18 17:51:09 +0100

    Fix Vma path

--- VK2D/VulkanInterface.h
+++ VK2D/VulkanInterface.h
@@ -4,7 +4,7 @@
 #pragma once
 #include "VK2D/Structs.h"
 #define VMA_VULKAN_VERSION 1002000
-#include <VulkanMemoryAllocator/src/VmaUsage.h>
+#include <VmaUsage.h>
 
 #ifdef __cplusplus
 extern "C" {
